94ccaaf576cdba728606e76f3b3197defaf468e4
electrolytej <electrolytej@gmail.com>
2025-06-18 22:58:17 +0800

    u

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
 plugins {
-    // Apply the shared build logic from a convention plugin.
-    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
     id("buildsrc.convention.kotlin-jvm") apply false
-
-    // Apply the Application plugin to add support for building an executable JVM application.
-    application apply false
+    id("com.squareup.wire") version "4.4.0" apply false
 }
 
---
+++ model/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+    // Apply Kotlin Serialization plugin from `gradle/libs.versions.toml`.
+    alias(libs.plugins.kotlinPluginSerialization)
+    id("com.squareup.wire")
+}
+
+wire {
+    kotlin {
+        // 可选：指定生成的包名
+//        javaPackage = 'com.example.protobuf'
+
+        // 可选：生成接口类
+        includes = ["com.example.messages.*"]
+    }
+    sourcePath {
+        srcDir += "src/main/protos" // 你的 .proto 文件目录
+    }
+
+    // 可选：排除某些 proto 文件
+    // prune 'google.protobuf.*'
+}
+
+dependencies {
+    // Apply the kotlinx bundle of dependencies from the version catalog (`gradle/libs.versions.toml`).
+    implementation(libs.bundles.kotlinxEcosystem)
+    testImplementation(kotlin("test"))
+    // 运行时库
+    implementation("com.squareup.wire:wire-runtime:4.4.0")
+    // 注解处理（生成代码）
+    kapt("com.squareup.wire:wire-compiler:4.4.0")
+//    annotationProcessor("com.squareup.wire:wire-compiler:4.4.0")
+}
