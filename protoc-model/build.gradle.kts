3cd9ecfaa7f682ac3ad7513b1405feb874648986
electrolytej <electrolytej@gmail.com>
2025-06-20 09:32:24 +0800

    u

--- protoc-model/build.gradle.kts
+++ protoc-model/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.protobuf.gradle.GenerateProtoTask
+import com.google.protobuf.gradle.remove
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
@@ -10,46 +12,34 @@ sourceSets {
     main {
         proto {
             srcDir("${rootDir}/proto")
-
         }
     }
 }
 protobuf {
-    generatedFilesBaseDir = "src" // 基础目录
     protoc {
         artifact = "com.google.protobuf:protoc:3.21.12" // 编译器版本
     }
-//    generateProtoTasks {
-//
-//        all().forEach { task ->
-//            // Java 类生成
-//            task.builtins.register("java") {
-//                outputSubDir = "src/main/kotlin"
-//            }
-//
-//            // gRPC 代码生成（作为插件）
+    generateProtoTasks {
+
+        all().forEach { task ->
+
+            task.builtins.names.forEach { name ->
+                task.builtins.remove(name)
+            }
+            task.builtins.removeAll { _: GenerateProtoTask.PluginOptions -> true }
+            task.builtins.create("java") {
+                println("cjf ${generatedFilesBaseDir}")
+                generatedFilesBaseDir = "${projectDir}/src/main"
+//                outputSubDir = "java"
+            }
+
+            // gRPC 代码生成（作为插件）
 //            task.plugins.create("grpc") {
 //                outputSubDir = "src/generated/main/grpc"
+//                option("lite")
 //            }
-//        }
-//    }
-//    generateProtoTasks {
-//        all().forEach { task ->
-//            // 配置 Java 生成器
-//            task.builtins {
-//
-//                create("java") {
-//                    // 设置新的输出目录（相对于项目根目录）
-//                    outputSubDir = "src/main/java"
-//                }
-//                // 配置 gRPC 生成器（可选）
-////                create("grpc") {
-////                    outputSubDir = "src/main/grpc"
-////                    option("lite")
-////                }
-//            }
-//        }
-//    }
+        }
+    }
 }
 kotlin {
     jvmToolchain(21)
